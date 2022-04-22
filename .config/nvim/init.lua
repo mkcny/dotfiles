8738a23822f0623752616b17fe867ab9f43b05db
Don Chea <donaldducky@gmail.com>
2022-04-22 02:35:44 -0400

    Convert key mappings to lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -62,53 +62,52 @@ vim.cmd [[colorscheme base16-onedark]]
 
 vim.g.mapleader = ','
 
+-- open the file under the cursor
+vim.keymap.set('n', '<leader>o', '<cmd>vertical wincmd f<cr>')
+
+-- search word under cursor
+vim.keymap.set('n', '<leader>f', '<cmd>Telescope lsp_references<cr>')
+
+-- show problems with telescope
+vim.keymap.set('n', '<leader>d', '<cmd>Telescope diagnostics<cr>')
+
+-- use telescope for ctrl+p search
+vim.keymap.set('n', '<c-p>', '<cmd>Telescope find_files<cr>')
+
+-- use fugitive to show the blame
+vim.keymap.set('n', '<leader>g', '<cmd>Git blame<cr>')
+
+-- easier quitting
+vim.keymap.set('n', '<c-q>', '<cmd>q<cr>')
+
+-- disable Ex mode
+vim.keymap.set('n', 'Q', '<nop>')
+
+-- backslash to search within project
+vim.keymap.set('n', '\\', '<cmd>Rg<cr>')
+
+-- enter key clears search highlighting
+vim.keymap.set('n', '<cr>', '<cmd>nohlsearch<cr>')
+
+-- close quickfix
+vim.keymap.set('n', '<space>', '<cmd>cclose<cr>')
+
+-- Move around splits with <C-hjkl>
+vim.keymap.set('n', '<c-h>', '<cmd>wincmd h<cr><cmd>wincmd =<cr>')
+vim.keymap.set('n', '<c-j>', '<cmd>wincmd j<cr><cmd>wincmd =<cr>')
+vim.keymap.set('n', '<c-k>', '<cmd>wincmd k<cr><cmd>wincmd =<cr>')
+vim.keymap.set('n', '<c-l>', '<cmd>wincmd l<cr><cmd>wincmd =<cr>')
+
+-- maintain visual selection after indenting
+vim.keymap.set('v', '<', '<gv', { remap = true })
+vim.keymap.set('v', '>', '>gv', { remap = true })
+
+-- toggle commenting for selected lines
+vim.keymap.set('', '<leader>/', '<plug>NERDCommenterToggle', { remap = true })
+vim.keymap.set('v', '<leader>/', '<plug>NERDCommenterToggle<cr>gv', { remap = true })
+
 vim.api.nvim_exec(
   [[
-" open the file under the cursor
-nnoremap <leader>o :vertical wincmd f<CR>
-
-" search word under cursor
-map <leader>f :Telescope lsp_references<CR>
-
-" show problems with telescope
-nnoremap <leader>d :Telescope diagnostics<CR>
-
-" use telescope for ctrl+p search
-nnoremap <silent> <c-p> :Telescope find_files<CR>
-
-" use fugitive to show the blame
-nnoremap <leader>g :Git blame<CR>
-
-" easier quitting
-nnoremap <C-q> :q<CR>
-
-" disable Ex mode
-nnoremap Q <Nop>
-
-" backslash to search within project
-nnoremap \ :Rg<cr>
-
-" enter key clears search highlighting
-nnoremap <CR> :nohlsearch<cr>
-
-" close quickfix
-nnoremap <space> :ccl<CR>
-
-" Move around splits with <C-hjkl>
-nnoremap <C-h> :wincmd h<CR>:wincmd =<CR>
-nnoremap <C-j> :wincmd j<CR>:wincmd =<CR>
-nnoremap <C-k> :wincmd k<CR>:wincmd =<CR>
-nnoremap <C-l> :wincmd l<CR>:wincmd =<CR>
-
-" maintain visual selection after indenting
-vmap < <gv
-vmap > >gv
-
-" toggle commenting for selected lines
-map <leader>/ <plug>NERDCommenterToggle<CR>
-vmap <leader>/ <plug>NERDCommenterToggle<CR>gv
-
-
 let g:lightline = {
       \ 'colorscheme': 'one',
       \ 'component_function': {
