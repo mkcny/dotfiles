f755ca541f16a06f420866b87d8bc062094b6001
Michael Cooney <mike.cooney@gmail.com>
2025-08-06 12:04:37 -0400

    more tweaks to minimal nvim config

--- .config/nvim-minimal/init.lua
+++ .config/nvim-minimal/init.lua
@@ -4,46 +4,77 @@ vim.g.mapleader = ","
 vim.opt.ts = 4
 vim.opt.winwidth = 120
 vim.opt.winborder = "rounded"
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.signcolumn = 'yes'
 
 vim.pack.add({
 	"https://github.com/neovim/nvim-lspconfig",
 	"https://github.com/nvim-treesitter/nvim-treesitter",
-	"https://github.com/echasnovski/mini.comment",
 	"https://github.com/nvim-lua/plenary.nvim",
 	"https://github.com/nvim-telescope/telescope.nvim",
 	"https://github.com/Saghen/blink.cmp",
 	"https://github.com/lukas-reineke/indent-blankline.nvim",
-	"https://github.com/folke/tokyonight.nvim",
+	"https://github.com/catppuccin/nvim",
+	"https://github.com/nvim-tree/nvim-web-devicons",
+	"https://github.com/nvim-lualine/lualine.nvim",
+	"https://github.com/tpope/vim-fugitive",
+	"https://github.com/tpope/vim-rhubarb",
+	"https://github.com/echasnovski/mini.diff",
 })
 
-vim.cmd("colorscheme tokyonight-moon")
-
-vim.lsp.enable({ "lua_ls", "rust_analyzer", "gleam", "taplo" })
-vim.diagnostic.config({ virtual_lines = true })
-vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
-
-require('nvim-treesitter.configs').setup({
-	ensure_installed = { "lua", "rust", "gleam", "ruby", "go", "toml" },
-})
-
-require 'lspconfig'.rust_analyzer.setup {
-	settings = {
-		["rust-analyzer"] = {
-			checkOnSave = {
-				command = "clippy"
-			}
-		}
-	}
-}
-
+require('mini.diff').setup()
+require('lualine').setup({})
 require("ibl").setup()
 
 require("blink.cmp").setup({
-	fuzzy = { implementation = "lua" },
 	keymap = {
 		preset = 'enter',
 		['<Tab>'] = { 'snippet_forward', 'select_next', 'fallback' },
 		['<S-Tab>'] = { 'snippet_backward', 'select_prev', 'fallback' },
+	},
+	signature = { enabled = true }
+})
+
+require('nvim-treesitter.configs').setup({
+	ensure_installed = { "lua", "rust", "gleam", "ruby", "go", "toml" },
+	highlight = { enable = true },
+})
+
+vim.cmd("colorscheme catppuccin-macchiato")
+
+vim.lsp.enable({ "lua_ls", "rust_analyzer", "gleam", "taplo", "ruby_lsp" })
+vim.diagnostic.config({
+	virtual_text = true,
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = "✘",
+			[vim.diagnostic.severity.WARN] = "▲",
+			[vim.diagnostic.severity.HINT] = "⚑",
+			[vim.diagnostic.severity.INFO] = "»",
+
+		},
+	},
+})
+
+vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
+
+vim.lsp.config("lua_ls", {
+	settings = {
+		Lua = {
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("", true),
+			}
+		}
+	}
+})
+
+vim.lsp.config('rust_analyzer', {
+	settings = {
+		["rust-analyzer"] = {
+			cargo = { features = "all" },
+			checkOnSave = { command = "clippy" },
+		},
 	},
 })
 
@@ -60,8 +91,9 @@ vim.keymap.set('n', '<leader>t', '<cmd>lua vim.lsp.inlay_hint.enable(not vim.lsp
 -- easier quitting
 vim.keymap.set('n', '<c-q>', '<cmd>q<cr>')
 
--- lsp code actions
+-- lsp keys
 vim.keymap.set('n', '<c-.>', "<cmd>lua vim.lsp.buf.code_action()<cr>")
+vim.keymap.set('n', 'gd', "<cmd>lua vim.lsp.buf.definition()<cr>")
 
 -- Move around splits with <C-hjkl>
 vim.keymap.set('n', '<c-h>', '<cmd>wincmd h<cr><cmd>wincmd =<cr>')
@@ -72,3 +104,9 @@ vim.keymap.set('n', '<c-l>', '<cmd>wincmd l<cr><cmd>wincmd =<cr>')
 -- maintain visual selection after indenting
 vim.keymap.set('v', '<', '<gv', { remap = true })
 vim.keymap.set('v', '>', '>gv', { remap = true })
+
+-- close quickfix
+vim.keymap.set('n', '<space>', '<cmd>cclose<cr>')
+
+-- enter key clears search highlighting
+vim.keymap.set('n', '<cr>', '<cmd>nohlsearch<cr>')
